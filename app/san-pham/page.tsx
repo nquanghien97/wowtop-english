8d357cbcca2df9eb931ced11ecce75caa5b4c5eb
quanghien <nquanghien97@gmail.com>
2024-11-04 16:38:12 +0700

    fix FE

--- app/san-pham/page.tsx
+++ app/san-pham/page.tsx
@@ -247,100 +247,95 @@ function Product() {
           <p className="w-2/3 text-xl text-[#002A9E]">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam harum officia cupiditate doloremque fugit assumenda, sint necessitatibus reprehenderit.</p>
         </div>
       </section>
-      <section className="mb-8 bg-[url('/nen-ct.webp')] lg:bg-[url('/artboard12.jpg')] md:h-[1080px] bg-[length:100%_100%]">
-        <div className="max-w-6xl m-auto relative h-full">
-          <Image src="/PT.webp" alt="" width={150} height={150} className="absolute bottom-0 right-0 max-lg:hidden" />
-          <div className="flex flex-col items-center justify-center text-2xl md:text-[40px] uppercase text-center py-4">
-            <div>
-              <p className="bg-text bg-clip-text text-transparent font-bold md:pt-4 pt-1">Công thức tối ưu</p>
-            </div>
-            <div>
-              <p className="md:pt-6 pt-1">
-                <span className="bg-text bg-clip-text text-transparent">Cho </span>
-                <span className="py-2 px-4 bg-gradient-to-r from-blue-900 via-blue-700 to-blue-500 rounded-md text-white">Chiều cao vượt đỉnh</span>
-              </p>
-            </div>
+      <section className="mb-8 bg-[url('/nen-ct.webp')] relative">
+        <div className="flex flex-col items-center justify-center text-[28px] lg:text-[1.5vw] uppercase text-center py-4 lg:absolute lg:top-0 lg:left-1/2 lg:transform lg:-translate-x-1/2">
+          <div>
+            <p className="bg-text bg-clip-text text-transparent font-bold">Công thức tối ưu</p>
           </div>
-          <div className="relative text-xs max-lg:hidden h-full">
-            <div className="text-[#002A9E]">
-              <p className="w-[240px] absolute 2xl:top-[80px] 2xl:left-[60px] xl:top-[100px] xl:left-[160px] lg:top-[80px] lg:left-[220px]">
-                Phát triển khung xương chắc khỏe & tăng chiều cao vượt trội với <strong>CBP & CPP</strong> tăng cường hoạt động của nguyên bào xương, Hàm lượng Canxi cao hàng đầu (1200mg), tỷ lệ vàng Canxi : Photo = 1,4:1 kết hợp với D3K2 & khoáng chất·giúp hấp thu Canxi tối đa.
-              </p>
-              <p className="w-[190px] absolute 2xl:top-[40px] 2xl:right-[100px] xl:top-[40px] xl:right-[200px] lg:top-[40px] lg:right-[220px]">
-                <strong>DHA và ARA </strong>
-                giúp tăng chỉ số trí tuệ, phát triển trí não & hệ thần kinh, phát triển thị lực & sáng mắt</p>
-              <p className="w-[204px] absolute 2xl:top-[220px] 2xl:right-[60px] xl:top-[220px] xl:right-[140px] lg:top-[220px] lg:right-[200px]">
-                <strong>Lactoferrin & IgG </strong>
-                tăng cường điều hòa miễn dịch, Giúp kháng khuẩn, kháng virus, bảo vệ cơ thể khỏi các tác nhân gây hại.
-              </p>
-              <p className="w-[190px] absolute 2xl:bottom-[440px] 2xl:left-[-40px] xl:bottom-[420px] xl:left-[80px] lg:bottom-[440px] lg:left-[140px]">
-                <strong>OPO</strong> giúp hệ tiêu hóa vận hành tốt hơn, nhờ những tác động tích cực đến hệ sinh thái 100.000 tỷ vi khuẩn đường ruột. Đồng thời được bổ sung đặc biệt với lợi khuẩn Bifido-bacterium BB-12 và chất xơ (FOS+GOS).
-              </p>
-              <p className="w-[190px] absolute 2xl:bottom-[400px] 2xl:right-[100px] xl:bottom-[400px] xl:right-[160px] lg:bottom-[400px] lg:right-[200px]">
-                Cung cấp <strong>chất béo</strong> và <strong>năng lượng</strong> bền bỉ như 1 bữa ăn đầy đủ dinh dưỡng. <strong>Protein từ Đạm Whey thủy phân</strong> với kích thước siêu nhỏ dễ hấp thu & giảm tỉ lệ dị ứng sữa.
-              </p>
-            </div>
+          <div>
+            <p className=" pt-1">
+              <span className="bg-text bg-clip-text text-transparent">Cho </span>
+              <span className="py-2 px-4 bg-gradient-to-r from-blue-900 via-blue-700 to-blue-500 rounded-md text-white">Chiều cao vượt đỉnh</span>
+            </p>
           </div>
-          <div className="lg:hidden text-xs">
-            <div className="flex flex-col justify-center items-center mt-2 ">
-              <Image src="/Artboard 13.png" alt="bg-hop-sua" width={300} height={290} />
-              <div className="flex flex-col px-2 mt-[-80px]">
-                <div className="flex justify-center">
-                  <div className="relative flex justify-center items-center mr-4">
-                    <Image src="/bong.webp" alt="bong" width={120} height={120} />
-                    <span className="text-[#84571B] text-center text-shadow font-bold absolute max-w-[100px]">Cao hơn, chắc khỏe hơn từ cấp nguyên bào xương</span>
-                  </div>
-                  <div className="md:w-3/4 w-2/3 flex items-center pr-4">
-                    <span className="text-[#84571B] font-semibold">
-                      Phát triển khung xương chắc khỏe & tăng chiều cao vượt trội với <strong>CBP</strong> (125 mg/100g) & <strong>CPP</strong> (571 mg/100g) tăng cường hoạt động của nguyên bào xương. Hàm lượng <strong>Canxi</strong> cao hàng đầu (1475 mg/100g), tỷ lệ vàng <strong>Canxi : Phospho</strong> = 1,8:1 kết hợp với D3K1 & khoáng chất·giúp hấp thu Canxi tối đa.
-                    </span>
-                  </div>
+        </div>
+        <Image src="/artboard12.jpg" alt="" width={1920} height={1080} className="max-lg:hidden w-full" />
+        <Image src="/PT.webp" alt="" width={150} height={150} className="absolute bottom-0 right-0 max-lg:hidden" />
+        <p className="text-xs max-lg:hidden text-[#002A9E] w-[240px] absolute top-[20%] left-[22%]">
+          Phát triển khung xương chắc khỏe & tăng chiều cao vượt trội với <strong>CBP & CPP</strong> tăng cường hoạt động của nguyên bào xương, Hàm lượng Canxi cao hàng đầu (1200mg), tỷ lệ vàng Canxi : Photo = 1,4:1 kết hợp với D3K2 & khoáng chất·giúp hấp thu Canxi tối đa.
+        </p>
+        <p className="text-xs max-lg:hidden text-[#002A9E] w-[190px] absolute top-[16%] 2xl:right-[26%] right-[20%]">
+          <strong>DHA và ARA </strong>
+          giúp tăng chỉ số trí tuệ, phát triển trí não & hệ thần kinh, phát triển thị lực & sáng mắt</p>
+        <p className="text-xs max-lg:hidden text-[#002A9E] w-[204px] absolute top-[46%] 2xl:right-[21%] right-[15%]">
+          <strong>Lactoferrin & IgG </strong>
+          tăng cường điều hòa miễn dịch, Giúp kháng khuẩn, kháng virus, bảo vệ cơ thể khỏi các tác nhân gây hại.
+        </p>
+        <p className="text-xs max-lg:hidden text-[#002A9E] w-[190px] absolute bottom-[28%] left-[16%] 2xl:left-[20%]">
+          <strong>OPO</strong> giúp hệ tiêu hóa vận hành tốt hơn, nhờ những tác động tích cực đến hệ sinh thái 100.000 tỷ vi khuẩn đường ruột. Đồng thời được bổ sung đặc biệt với lợi khuẩn Bifido-bacterium BB-12 và chất xơ (FOS+GOS).
+        </p>
+        <p className="text-xs max-lg:hidden text-[#002A9E] w-[190px] absolute bottom-[24%] 2xl:bottom-[26%] 2xl:right-[22%] right-[18%]">
+          Cung cấp <strong>chất béo</strong> và <strong>năng lượng</strong> bền bỉ như 1 bữa ăn đầy đủ dinh dưỡng. <strong>Protein từ Đạm Whey thủy phân</strong> với kích thước siêu nhỏ dễ hấp thu & giảm tỉ lệ dị ứng sữa.
+        </p>
+        <div className="lg:hidden text-xs">
+          <div className="flex flex-col justify-center items-center mt-2 ">
+            <Image src="/Artboard 13.png" alt="bg-hop-sua" width={300} height={290} />
+            <div className="flex flex-col px-2 mt-[-80px]">
+              <div className="flex">
+                <div className="md:w-1/4 w-1/3 relative flex justify-center items-center">
+                  <Image src="/bong.webp" alt="bong" width={120} height={120} />
+                  <span className="text-[#002A9E] text-center text-shadow font-bold absolute max-w-[100px]">Cao hơn, chắc khỏe hơn từ cấp nguyên bào xương</span>
                 </div>
-                <div className="flex justify-center">
-                  <div className="relative flex justify-center items-center mr-4">
-                    <Image src="/bong.webp" alt="bong" width={120} height={120} />
-                    <span className="text-[#84571B] text-center text-shadow font-bold absolute max-w-[100px]">Tiêu hoá Tốt</span>
-                  </div>
-                  <div className="md:w-3/4 w-2/3 flex items-center pr-4">
-                    <span className="text-[#84571B] font-semibold">
-                      <strong>OPO</strong> giúp hệ tiêu hóa vận hành tốt hơn, nhờ những tác động tích cực đến hệ sinh thái 100.000 tỷ vi khuẩn đường ruột. Đồng thời được bổ sung đặc biệt với lợi khuẩn Bifido-bacterium BB-12 và chất xơ (FOS+GOS).
-                    </span>
-                  </div>
+                <div className="md:w-3/4 w-2/3 flex items-center pr-4">
+                  <span className="text-[#002A9E] font-semibold">
+                    Phát triển khung xương chắc khỏe & tăng chiều cao vượt trội với <strong>CBP</strong> (125 mg/100g) & <strong>CPP</strong> (571 mg/100g) tăng cường hoạt động của nguyên bào xương. Hàm lượng <strong>Canxi</strong> cao hàng đầu (1475 mg/100g), tỷ lệ vàng <strong>Canxi : Phospho</strong> = 1,8:1 kết hợp với D3K1 & khoáng chất·giúp hấp thu Canxi tối đa.
+                  </span>
                 </div>
-                <div className="flex justify-center">
-                  <div className="relative flex justify-center items-center mr-4">
-                    <Image src="/bong.webp" alt="bong" width={120} height={120} />
-                    <span className="text-[#84571B] text-center text-shadow font-bold absolute max-w-[100px]">Cung cấp dinh dưỡng toàn diện</span>
-                  </div>
-                  <div className="md:w-3/4 w-2/3 flex items-center pr-4">
-                    <span className="text-[#84571B] font-semibold">
-                      Cung cấp <strong>chất béo</strong> và <strong>năng lượng</strong> bền bỉ như 1 bữa ăn đầy đủ dinh dưỡng. <strong>Protein từ Đạm Whey thủy phân</strong> với kích thước siêu nhỏ dễ hấp thu & giảm tỉ lệ dị ứng sữa.
-                    </span>
-                  </div>
+              </div>
+              <div className="flex">
+                <div className="md:w-1/4 w-1/3 relative flex justify-center items-center">
+                  <Image src="/bong.webp" alt="bong" width={120} height={120} />
+                  <span className="text-[#002A9E] text-center text-shadow font-bold absolute max-w-[100px]">Tiêu hoá Tốt</span>
                 </div>
-                <div className="flex justify-center">
-                  <div className="relative flex justify-center items-center mr-4">
-                    <Image src="/bong.webp" alt="bong" width={120} height={120} />
-                    <span className="text-[#84571B] text-center text-shadow font-bold absolute max-w-[100px]">Phát triển trí não & sáng mắt</span>
-                  </div>
-                  <div className="md:w-3/4 w-2/3 flex items-center pr-4">
-                    <span className="text-[#84571B] font-semibold">
-                      <strong>DHA và ARA </strong>
-                      giúp tăng chỉ số trí tuệ, phát triển trí não & hệ thần kinh, phát triển thị lực & sáng mắt
-                    </span>
-                  </div>
+                <div className="md:w-3/4 w-2/3 flex items-center pr-4">
+                  <span className="text-[#002A9E] font-semibold">
+                    <strong>OPO</strong> giúp hệ tiêu hóa vận hành tốt hơn, nhờ những tác động tích cực đến hệ sinh thái 100.000 tỷ vi khuẩn đường ruột. Đồng thời được bổ sung đặc biệt với lợi khuẩn Bifido-bacterium BB-12 và chất xơ (FOS+GOS).
+                  </span>
                 </div>
-                <div className="flex justify-center">
-                  <div className="relative flex justify-center items-center mr-4">
-                    <Image src="/bong.webp" alt="bong" width={120} height={120} />
-                    <span className="text-[#84571B] text-center text-shadow font-bold absolute max-w-[100px]">Tăng sức đề kháng</span>
-                  </div>
-                  <div className="md:w-3/4 w-2/3 flex items-center pr-4">
-                    <span className="text-[#84571B] font-semibold">
-                      <strong>Lactoferrin & IgG </strong>
-                      tăng cường điều hòa miễn dịch, Giúp kháng khuẩn, kháng virus, bảo vệ cơ thể khỏi các tác nhân gây hại.
-                    </span>
-                  </div>
+              </div>
+              <div className="flex">
+                <div className="md:w-1/4 w-1/3 relative flex justify-center items-center">
+                  <Image src="/bong.webp" alt="bong" width={120} height={120} />
+                  <span className="text-[#002A9E] text-center text-shadow font-bold absolute max-w-[100px]">Cung cấp dinh dưỡng toàn diện</span>
+                </div>
+                <div className="md:w-3/4 w-2/3 flex items-center pr-4">
+                  <span className="text-[#002A9E] font-semibold">
+                    Cung cấp <strong>chất béo</strong> và <strong>năng lượng</strong> bền bỉ như 1 bữa ăn đầy đủ dinh dưỡng. <strong>Protein từ Đạm Whey thủy phân</strong> với kích thước siêu nhỏ dễ hấp thu & giảm tỉ lệ dị ứng sữa.
+                  </span>
+                </div>
+              </div>
+              <div className="flex">
+                <div className="md:w-1/4 w-1/3 relative flex justify-center items-center">
+                  <Image src="/bong.webp" alt="bong" width={120} height={120} />
+                  <span className="text-[#002A9E] text-center text-shadow font-bold absolute max-w-[100px]">Phát triển trí não & sáng mắt</span>
+                </div>
+                <div className="md:w-3/4 w-2/3 flex items-center pr-4">
+                  <span className="text-[#002A9E] font-semibold">
+                    <strong>DHA và ARA </strong>
+                    giúp tăng chỉ số trí tuệ, phát triển trí não & hệ thần kinh, phát triển thị lực & sáng mắt
+                  </span>
+                </div>
+              </div>
+              <div className="flex">
+                <div className="md:w-1/4 w-1/3 relative flex justify-center items-center">
+                  <Image src="/bong.webp" alt="bong" width={120} height={120} />
+                  <span className="text-[#002A9E] text-center text-shadow font-bold absolute max-w-[100px]">Tăng sức đề kháng</span>
+                </div>
+                <div className="md:w-3/4 w-2/3 flex items-center pr-4">
+                  <span className="text-[#002A9E] font-semibold">
+                    <strong>Lactoferrin & IgG </strong>
+                    tăng cường điều hòa miễn dịch, Giúp kháng khuẩn, kháng virus, bảo vệ cơ thể khỏi các tác nhân gây hại.
+                  </span>
                 </div>
               </div>
             </div>
