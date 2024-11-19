13e274e29eb1b613e4c3e404eb10d5dfad0cc03d
quanghien <nquanghien97@gmail.com>
2024-11-19 09:05:43 +0700

    fix FE

--- app/san-pham/page.tsx
+++ app/san-pham/page.tsx
@@ -16,6 +16,7 @@ const data = [
     moi_khau_phan: '663',
     tren_100g: '1902',
     tren_100kJ: '100',
+    is_bold: false
   },
   {
     id: 2,
@@ -24,6 +25,7 @@ const data = [
     moi_khau_phan: '4.9',
     tren_100g: '14.1',
     tren_100kJ: '0.7',
+    is_bold: false
   },
   {
     id: 3,
@@ -32,6 +34,7 @@ const data = [
     moi_khau_phan: '4.8',
     tren_100g: '14.0',
     tren_100kJ: '0.7',
+    is_bold: false
   },
   {
     id: 4,
@@ -40,6 +43,7 @@ const data = [
     moi_khau_phan: '6.0',
     tren_100g: '17.3',
     tren_100kJ: '0.9',
+    is_bold: false
   },
   {
     id: 5,
@@ -48,6 +52,7 @@ const data = [
     moi_khau_phan: '882',
     tren_100g: '2551',
     tren_100kJ: '134',
+    is_bold: false
   },
   {
     id: 6,
@@ -56,6 +61,7 @@ const data = [
     moi_khau_phan: '78.2',
     tren_100g: '224',
     tren_100kJ: '11.8',
+    is_bold: false
   },
   {
     id: 7,
@@ -64,6 +70,7 @@ const data = [
     moi_khau_phan: '810',
     tren_100g: '2337',
     tren_100kJ: '122',
+    is_bold: true
   },
   {
     id: 8,
@@ -72,6 +79,7 @@ const data = [
     moi_khau_phan: '20.5',
     tren_100g: '58.0',
     tren_100kJ: '3.0',
+    is_bold: false
   },
   {
     id: 9,
@@ -80,6 +88,7 @@ const data = [
     moi_khau_phan: '20.2',
     tren_100g: '58.0',
     tren_100kJ: '3.0',
+    is_bold: false
   },
   {
     id: 10,
@@ -88,6 +97,7 @@ const data = [
     moi_khau_phan: '337',
     tren_100g: '2400',
     tren_100kJ: '126',
+    is_bold: false
   },
   {
     id: 11,
@@ -96,6 +106,7 @@ const data = [
     moi_khau_phan: '458',
     tren_100g: '3154',
     tren_100kJ: '169',
+    is_bold: false
   },
   {
     id: 12,
@@ -104,6 +115,7 @@ const data = [
     moi_khau_phan: '379',
     tren_100g: '2400',
     tren_100kJ: '126',
+    is_bold: false
   },
   {
     id: 13,
@@ -112,6 +124,7 @@ const data = [
     moi_khau_phan: '144',
     tren_100g: '412',
     tren_100kJ: '21.7',
+    is_bold: false
   },
   {
     id: 14,
@@ -120,6 +133,7 @@ const data = [
     moi_khau_phan: '2.0',
     tren_100g: '5.7',
     tren_100kJ: '0.30',
+    is_bold: true
   },
   {
     id: 15,
@@ -128,6 +142,7 @@ const data = [
     moi_khau_phan: '3.1',
     tren_100g: '9.0',
     tren_100kJ: '0.47',
+    is_bold: false
   },
   {
     id: 16,
@@ -136,6 +151,7 @@ const data = [
     moi_khau_phan: '10.0',
     tren_100g: '28.7',
     tren_100kJ: '1.5',
+    is_bold: true
   },
   {
     id: 17,
@@ -144,6 +160,7 @@ const data = [
     moi_khau_phan: '153',
     tren_100g: '438',
     tren_100kJ: '23',
+    is_bold: false
   },
   {
     id: 18,
@@ -152,6 +169,7 @@ const data = [
     moi_khau_phan: '368',
     tren_100g: '1055',
     tren_100kJ: '55.5',
+    is_bold: false
   },
   {
     id: 19,
@@ -160,6 +178,7 @@ const data = [
     moi_khau_phan: '106',
     tren_100g: '303',
     tren_100kJ: '15.9',
+    is_bold: false
   },
   {
     id: 20,
@@ -168,6 +187,7 @@ const data = [
     moi_khau_phan: '1.0',
     tren_100g: '2.8',
     tren_100kJ: '0.15',
+    is_bold: false
   },
   {
     id: 21,
@@ -176,6 +196,7 @@ const data = [
     moi_khau_phan: '44.8',
     tren_100g: '129',
     tren_100kJ: '6.8',
+    is_bold: false
   },
   {
     id: 22,
@@ -184,6 +205,7 @@ const data = [
     moi_khau_phan: '1237',
     tren_100g: '3551',
     tren_100kJ: '187',
+    is_bold: false
   },
   {
     id: 23,
@@ -192,6 +214,7 @@ const data = [
     moi_khau_phan: '905',
     tren_100g: '2598',
     tren_100kJ: '137',
+    is_bold: false
   },
   {
     id: 24,
@@ -200,6 +223,7 @@ const data = [
     moi_khau_phan: '15.4',
     tren_100g: '44.2',
     tren_100kJ: '2.3',
+    is_bold: false
   },
   {
     id: 25,
@@ -208,6 +232,7 @@ const data = [
     moi_khau_phan: '8.7',
     tren_100g: '25.1',
     tren_100kJ: '1.3',
+    is_bold: false
   },
   {
     id: 26,
@@ -216,62 +241,160 @@ const data = [
     moi_khau_phan: '55.2',
     tren_100g: '158',
     tren_100kJ: '8.3',
+    is_bold: false
   },
   {
     id: 27,
-    muc: 'Kali',
-    don_vi: 'mg',
-    moi_khau_phan: '177',
-    tren_100g: '508',
-    tren_100kJ: '26.7',
-  },
-  {
-    id: 28,
     muc: 'Phốt pho',
     don_vi: 'mg',
     moi_khau_phan: '284',
     tren_100g: '814',
     tren_100kJ: '42.8',
+    is_bold: false
+  },
+  {
+    id: 28,
+    muc: 'Kali',
+    don_vi: 'mg',
+    moi_khau_phan: '177',
+    tren_100g: '508',
+    tren_100kJ: '26.7',
+    is_bold: false
   },
   {
     id: 29,
-    muc: 'Canxi',
+    muc: 'Magie',
+    don_vi: 'mg',
+    moi_khau_phan: '14.6',
+    tren_100g: '41.9',
+    tren_100kJ: '2.2',
+    is_bold: false
+  },
+  {
+    id: 30,
+    muc: 'Calci',
     don_vi: 'mg',
     moi_khau_phan: '514',
     tren_100g: '1475',
     tren_100kJ: '77.5',
-  },
-  {
-    id: 30,
-    muc: 'Kẽm',
-    don_vi: 'mg',
-    moi_khau_phan: '1.0',
-    tren_100g: '2.9',
-    tren_100kJ: '0.15',
+    is_bold: true
   },
   {
     id: 31,
+    muc: 'Ca:P',
+    don_vi: '',
+    moi_khau_phan: '1.8:1',
+    tren_100g: '1.8:1',
+    tren_100kJ: '1.8:1',
+    is_bold: true
+  },
+  {
+    id: 32,
     muc: 'Sắt',
     don_vi: 'mg',
     moi_khau_phan: '1.8',
     tren_100g: '5.3',
     tren_100kJ: '0.28',
+    is_bold: false
   },
   {
-    id: 32,
+    id: 33,
+    muc: 'Kẽm',
+    don_vi: 'mg',
+    moi_khau_phan: '1.0',
+    tren_100g: '2.9',
+    tren_100kJ: '0.15',
+    is_bold: false
+  },
+  {
+    id: 34,
+    muc: 'Iốt',
+    don_vi: 'μg',
+    moi_khau_phan: '28.1',
+    tren_100g: '80.7',
+    tren_100kJ: '4.2',
+    is_bold: false
+  },
+  {
+    id: 35,
+    muc: 'Selen',
+    don_vi: 'μg',
+    moi_khau_phan: '4.8',
+    tren_100g: '13.7',
+    tren_100kJ: '0.7',
+    is_bold: false
+  },
+  {
+    id: 36,
+    muc: 'Đồng',
+    don_vi: 'μg',
+    moi_khau_phan: '77.1',
+    tren_100g: '221',
+    tren_100kJ: '11.6',
+    is_bold: false
+  },
+  {
+    id: 37,
+    muc: 'Clorua',
+    don_vi: 'mg',
+    moi_khau_phan: '125',
+    tren_100g: '359',
+    tren_100kJ: '18.9',
+    is_bold: false
+  },
+  {
+    id: 38,
+    muc: 'Mangan',
+    don_vi: 'μg',
+    moi_khau_phan: '21.9',
+    tren_100g: '62.8',
+    tren_100kJ: '3.3',
+    is_bold: false
+  },
+  {
+    id: 39,
+    muc: 'Lactoferrin',
+    don_vi: 'mg',
+    moi_khau_phan: '36.4',
+    tren_100g: '104',
+    tren_100kJ: '5.5',
+    is_bold: true
+  },
+  {
+    id: 40,
+    muc: 'Choline',
+    don_vi: 'mg',
+    moi_khau_phan: '37.0',
+    tren_100g: '106',
+    tren_100kJ: '5.6',
+    is_bold: false
+  },
+  {
+    id: 41,
+    muc: 'DHA',
+    don_vi: 'mg',
+    moi_khau_phan: '28.1',
+    tren_100g: '80.7',
+    tren_100kJ: '4.2',
+    is_bold: false
+  },
+  {
+    id: 42,
     muc: 'ARA',
     don_vi: 'mg',
     moi_khau_phan: '28.1',
     tren_100g: '80.7',
     tren_100kJ: '4.2',
+    is_bold: false
   },
   {
-    id: 33,
-    muc: 'DHA',
+    id: 43,
+    muc: 'Immunoglobulin G',
     don_vi: 'mg',
-    moi_khau_phan: '36.4',
-    tren_100g: '104',
-    tren_100kJ: '5.6',
+    moi_khau_phan: '7.5',
+    tren_100g: '21.5',
+    tren_100kJ: '1.1',
+    is_bold: true
   },
 ];
 
@@ -652,7 +775,7 @@ function Product() {
             </thead>
             <tbody className="block w-full h-[400px] overflow-auto">
               {data.map(item => (
-                <tr className="text-[#065691] cursor-pointer hover:bg-[#f7ecd0] duration-300 h-[40px] w-full block" key={item.id}>
+                <tr className={`text-[#065691] my-[1px] cursor-pointer ${item.is_bold ? 'bg-[#f7ecd0]' : 'hover:bg-[#f7ecd0]'} duration-300 h-[40px] w-full block`} key={item.id}>
                   <td className="text-[#84571B] py-2 w-1/5 inline-block">{item.muc}</td>
                   <td className="text-[#84571B] py-2 w-1/5 inline-block">{item.don_vi}</td>
                   <td className="text-[#84571B] py-2 w-1/5 inline-block">{item.moi_khau_phan}</td>
